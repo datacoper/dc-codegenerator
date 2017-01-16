1e07818b58bd44ac4aa0fc4b3773ab5d0b90b035
Roberto Filho <roberto.w.filho@gmail.com>
2017-01-16 17:14:55 -0200

    Organizando imports. O IntelliJ não acusa imports não utilizados.

--- src/main/java/com/datacoper/maven/generators/impl/ValidatorGenerator.java
+++ src/main/java/com/datacoper/maven/generators/impl/ValidatorGenerator.java
@@ -23,8 +23,7 @@ public class ValidatorGenerator extends AbstractGenerator<TClass> {
 
     public String getPackage() {
         String name = data.getClassName().toLowerCase();
-        String module = DCProjectUtil.getModuleName(project);
-        
+
         return StringUtil.format("com.{0}.cooperate.{1}.server.{2}", data.getCompany().getPackag(), getModuleToPackage(), name);
     }
     
--- src/main/java/com/datacoper/maven/generators/impl/ValidatorTestGenerator.java
+++ src/main/java/com/datacoper/maven/generators/impl/ValidatorTestGenerator.java
@@ -24,8 +24,7 @@ public class ValidatorTestGenerator extends AbstractGenerator<TClass> {
 
     public String getPackage() {
         String name = data.getClassName().toLowerCase();
-        String module = DCProjectUtil.getModuleName(project);
-        
+
         return StringUtil.format("com.{0}.cooperate.{1}.server.{2}", data.getCompany().getPackag(), getModuleToPackage(), name);
     }
     
