9db3f4a80600e62ec2593bb45b153d311846cac4
Lucas Didur <lucasdidur@hotmail.com>
2018-10-16 10:25:06 -0300

    Correção local do arquivo de implementação do resource do detail

--- src/main/java/com/datacoper/maven/generators/impl/ResourceImplDetailGenerator.java
+++ src/main/java/com/datacoper/maven/generators/impl/ResourceImplDetailGenerator.java
@@ -17,7 +17,7 @@ public class ResourceImplDetailGenerator extends AbstractJavaDetailGenerator {
 
     @Override
     public String getPackage() {
-        return StringUtil.format("com.{0}.cooperate.{1}.rest.resources.impl", getCompany().getPackageName(), getModulePackageName());
+        return StringUtil.format("com.{0}.{1}.rest.resources.impl", getCompany().getPackageName(), getModulePackageName());
     }
 
 	@Override
